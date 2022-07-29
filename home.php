22a744165a5551f9aa5183b325bb48220686e28f
Arnaldo Reyes <arnaldoreyes13@gmail.com>
2022-07-29 15:44:59 -0400

    Footer

--- home.php
+++ home.php
@@ -13,14 +13,71 @@
     <section class="header">
         <a href="./home.php" class="logo">Travel.</a>
         <nav class="navbar">
-            <a href="./home.php">Inicio</a>
-            <a href="./about.php">Acerca de</a>
-            <a href="./package.php">Paquetes</a>
-            <a href="./book.php">Reservaciones</a>
+            <a href="./home.php">inicio</a>
+            <a href="./about.php">acerca de</a>
+            <a href="./package.php">paquetes</a>
+            <a href="./book.php">reservar</a>
         </nav>
         <div id="menu-btn" class="fas fa-bars"></div>
     </section>
+
+ 
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>enlaces directos</h3>
+                <a href="./home.php"><i class="fas fa-angle-right"></i> inicio</a>
+                <a href="./about.php"><i class="fas fa-angle-right"></i> acerca de</a>
+                <a href="./package.php"><i class="fas fa-angle-right"></i> paquetes</a>
+                <a href="./book.php"><i class="fas fa-angle-right"></i> reserva</a>
+            </div>
+
+            <div class="box">
+                <h3>enlaces adicionales</h3>
+                <a href="#"><i class="fas fa-angle-right"></i> hacer preguntas</a>
+                <a href="#"><i class="fas fa-angle-right"></i> acerca de nostros</a>
+                <a href="#"><i class="fas fa-angle-right"></i> política de privacidad</a>
+                <a href="#"><i class="fas fa-angle-right"></i> términos de uso</a>
+            </div>
+
+            <div class="box">
+                <h3>información de contacto</h3>
+                <a href="#"><i class="fas fa-phone"></i> +56-123-4567890</a>
+                <a href="#"><i class="fas fa-phone"></i> +56-333-4444444</a>
+                <a href="#"><i class="fas fa-envelope"></i> tourandtravel1@fmail.com</a>
+                <a href="#"><i class="fas fa-map"></i> Nueva Esparta, Venezuela - 6316</a>    
+            </div>
+            
+            <div class="box">
+                <h3>Siguenos</h3>
+                <a href="#"><i class="fab fa-facebook"></i> facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i> twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i> instagram</a>
+                <a href="#"><i class="fab fa-linkedin"></i> linkedin</a>
+            </div>
+
+        </div>
+
+        <div class="credit"> creado por <span> Arnaldo Reyes </span> | © 2022 </div>
+
+    </section>
+
+
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
     <script src="./js/script.js"></script>
 </body>
